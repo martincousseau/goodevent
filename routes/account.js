4422d249f9b9c12dedda5d30fd24156c6ed8bc8f
martin <martin.cousseau2001@gmail.com>
2024-11-07 11:02:22 +0100

    ajout bonton pour aller vers mon compte + récupération des infos de l'utilisateur courant

--- routes/account.js
+++ routes/account.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const router = express.Router();
+const { getCurrentUser } = require('../controllers/authController');
 
-// Route pour afficher la page d'accueil
-router.get('/', (req, res) => {
+
+// Route pour afficher la page mon compte
+router.post('/', (req, res) => {
+
+    //TO DO
+    // liste des events créés par l'utilisateur, bouton qui renvoi vers les favoris de l'utilisateur 
+    const user = getCurrentUser(req);
     console.log('Rendering account page...');
     console.log('user : ', user)
-    res.render('account', { title: 'My Account', user: req.session.user });
+    res.render('account', { title: 'My Account', user:user});
 });
 
 
