d0bfa2eb66002c2d593e171c6fd0ede4fa941527
BORNY <zoe.borny@capgemini.com>
2024-11-08 10:12:31 +0100

    clean

--- routes/account.js
+++ routes/account.js
@@ -6,9 +6,7 @@ const { getEvents } = require('../controllers/eventController');
 router.get('/', async (req, res) => {
     console.log('Rendering My Account page...');
     const user = getCurrentUser(req);
-    console.log('user.id: ', user._id);
     const user_events = await getEvents(user._id);
-    console.log('user_events ', user_events);
 
     res.render('account', { title: 'My Account', user: user, user_events: user_events });
 });
