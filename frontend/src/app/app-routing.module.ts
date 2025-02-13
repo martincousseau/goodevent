e33e614c65b9535cdd79019187242c82a9d7e865
Zoé Borny <zoe.borny@gmail.com>
2025-02-13 15:10:59 +0100

    clean

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
 import { AccountComponent } from './pages/account/account.component';
-import { EventComponent } from './pages/event/event.component';
+import { EventComponent } from './components/event/event.component';
 import { EditEventComponent } from './pages/edit-event/edit-event.component';
 import { AuthGuard } from './guards/auth.guard';
 import { CreateEventComponent } from './pages/create-event/create-event.component';
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,14 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { AccountComponent } from './pages/account/account.component';
-import { EventComponent } from './pages/event/event.component';
+import { EventComponent } from './components/event/event.component';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
 import { EditEventComponent } from './pages/edit-event/edit-event.component';
 import { HttpClientModule } from '@angular/common/http';
 import { EventService } from './services/event.service';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { MenuComponent } from './pages/menu/menu.component';
+import { MenuComponent } from './components/menu/menu.component';
 import { CreateEventComponent } from './pages/create-event/create-event.component';
 import { SearchBarComponent } from './components/search-bar/search-bar.component';
 
